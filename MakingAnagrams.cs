1dfe51cdbf483d604bec71975a2da87c39d2f4d7
Oguzhan Topcu <oguzhntopcu@gmail.com>
2021-04-15 18:28:27 +0300

    Create MakingAnagrams.cs

---
+++ MakingAnagrams.cs
@@ -0,0 +1,41 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    static int makeAnagram(string a, string b)
+    {
+        var countMap = new int[256];
+        
+        foreach (var elem in a) countMap[elem]++;
+        foreach (var elem in b) countMap[elem]--;
+        
+        return countMap.Sum(Math.Abs);
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string a = Console.ReadLine();
+
+        string b = Console.ReadLine();
+
+        int res = makeAnagram(a, b);
+
+        textWriter.WriteLine(res);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
