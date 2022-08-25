067456fb1c7c06d3071ae2459f68178d1ffcdc5a
Oguzhan Topcu <oguzhntopcu@gmail.com>
2022-08-25 22:30:53 +0300

    Create MinimumAbsoluteDifference.cs

---
+++ MinimumAbsoluteDifference.cs
@@ -0,0 +1,51 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int minimumAbsoluteDifference(List<int> arr)
+    {
+        arr.Sort();
+        
+        var minDiff = int.MaxValue;
+        
+        for (var i = 1; i < arr.Count; i++) 
+        {
+            var diff = Math.Abs(arr[i -1] - arr[i]);
+            
+            if (minDiff > diff) minDiff = diff;
+        }
+        
+        return minDiff;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.minimumAbsoluteDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
